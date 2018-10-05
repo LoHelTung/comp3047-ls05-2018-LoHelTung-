1dd0d00a0912eab116974fb9da64ad4ee42c846f
16220714 <16220714@cs12197.comp.hkbu.edu.hk>
2018-10-05 15:52:35 +0800

    add person getInvalidID function

--- api/models/Person.js
+++ api/models/Person.js
@@ -12,13 +12,6 @@ module.exports = {
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-    name: {
-      type: "string"
-    },
-    
-    age: {
-      type: "string"
-    },
     
 
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
@@ -31,6 +24,14 @@ module.exports = {
     //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
 
   },
+  getInvalidIdMsg: function (opts) {
+
+    if (typeof opts.id === "undefined" || isNaN(parseInt(opts.id)))
+      return "Person id not specified or with incorrect type.";
+
+    return null;        // falsy
+
+},
 
 };
 
